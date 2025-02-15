ff1b974aa80068240c02b74a7a7beff956b3c5fd
NP <nadezdapratashchyk@gmail.com>
2025-02-15 22:46:05 +0100

    exersize-1.js

---
+++ 8kyu/Neutralisation.js
@@ -0,0 +1,9 @@
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+//
+//     When positives and positives interact, they remain positive.
+//     When negatives and negatives interact, they remain negative.
+//     But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+function neutralise(s1, s2) {
+    return str1.split("").map((char, i) => (char === str2[i] ? char : "0")).join("");
+}
